50328736898fa5d69801c7701026c0a2646a2045
Hayden Couch <hcouch@lcs.on.ca>
2022-01-18 09:26:00 -0500

    Initial Commit

---
+++ TrekrMultiPlatform/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrekrMultiPlatform
+//
+//  Created by Hayden Couch on 2022-01-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrekrMultiPlatform/TrekrMultiPlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrMultiPlatformApp.swift
+//  TrekrMultiPlatform
+//
+//  Created by Hayden Couch on 2022-01-18.
+//
+
+import SwiftUI
+
+@main
+struct TrekrMultiPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
